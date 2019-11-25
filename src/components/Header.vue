a0973b40bdf8c34dd217f0331977d4dbd068dfe2
Flash <188813950@qq.com>
2019-11-25 22:33:46 +0800

    初始化，加入cnode图标

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <img src="../logo.svg" />
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+
+
+
+<style scoped>
+</style>
