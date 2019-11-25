424ac214441e939b5fd29d75073daf842e3c3d63
Flash <188813950@qq.com>
2019-11-26 00:49:45 +0800

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="wrapper">
     <img alt="CNODE社区" src="../assets/logo.svg" />
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
   </div>
 </template>
 
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
 
 };
 </script>
 
 
 <style scoped>
+img{
+  padding: 0 20px 4px;
+  max-width: 150px;
+}
+.wrapper{
+  box-shadow: 0 1px 3px rgba(26,26,26,.1);
+  background-color:#24292E;
+  display: flex;
+  height: 60px;
+  justify-content: space-between;
+}
+ul{
+  margin-right: 32px;
+}
+li{
+  display: inline-block;
+  padding: 6px 10px 0;
+  font-size: 16px;
+}
+a{
+  text-decoration: none;
+  color:#FFFFFF;
+  font-weight: 600;
+}
 </style>
