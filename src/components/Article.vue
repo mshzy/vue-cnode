c0531eac11cb09b8cc50118463bd1a7978e92cb0
Flash <188813950@qq.com>
2019-11-28 23:54:55 +0800

    调整样式，添加回首页功能

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -3,27 +3,27 @@
     <div class="topic_header">
       <div class="topic_title">{{ res.title}}</div>
       <ul>
-        <li>•发布于：{{res.create_at | formatDate}}</li>
-        <li>•作者：{{res.author.loginname}}</li>
+        <li>•发布于:{{res.create_at | formatDate}}</li>
+        <li>•作者:{{res.author.loginname}}</li>
         <li>•{{res.visit_count}}次浏览</li>
       </ul>
-      <div v-html=" res.content" class="topic_content"></div>
+      <div v-html="res.content" class="topic_content"></div>
     </div>
     <div id="reply">
       <div class="topbar">回复</div>
       <div v-for="(reply,index) in res.replies" class="replySec" :key="index">
         <div class="replyUp">
           <router-link :to="{name:'user_info', params:{name:reply.author.loginname}}">
-            <img :src="reply.author.avatar_url" alt />
+            <img :src="reply.author.avatar_url" />
           </router-link>
           <router-link :to="{name:'user_info',params:{name:reply.author.loginname}}">
             <span>{{reply.author.loginname}}</span>
           </router-link>
           <span>{{index+1}}楼</span>
-          <span v-if="reply.ups.length>0">☝ {{reply.ups.length}}</span>
+          <span v-if="reply.ups.length > 0" class="upCount">☝{{reply.ups.length}}</span>
           <span v-else></span>
         </div>
-        <div v-html="reply.content"></div>
+        <div v-html="reply.content" class="replyContent"></div>
       </div>
     </div>
   </div>
@@ -40,38 +40,38 @@ export default {
   created() {
     this.axios.get(`https://cnodejs.org/api/v1/topic/${this.$route.params.id}`)
       .then(res => this.res = res.data.data)
-      .catch(error=>alert(error))
+      .catch(error => alert(error))
   }
 }
 </script>
 
 <style scoped>
-@import url("../assets/markdown-github.css");
 .topbar {
-  padding: 10px;
-  background-color: #f6f6f6;
-  height: 16px;
-  font-size: 12px;
-  margin-top: 10px;
+  background-color: #f6f8fa;
+  padding: 8px 0 6px 12px;
+  font-size: 13px;
+  font-weight: 400;
 }
 
-.article:not(:first-child) {
-  margin-right: 340px;
-  margin-top: 15px;
+.article {
+  margin: 30px 400px 30px 60px;
+  font-size: 15px;
 }
 
 #reply,
 .topic_header {
   background-color: #fff;
+  border: 1px solid #d1d5da;
 }
 
 #reply {
-  margin-top: 15px;
+  position: relative;
+  margin-top: 30px;
+  font-weight: 400;
 }
 
 #reply img {
-  width: 30px;
-  height: 30px;
+  width: 28px;
   position: relative;
   bottom: -9px;
 }
@@ -83,28 +83,32 @@ export default {
   text-decoration: none;
 }
 .replySec {
-  border-bottom: 1px solid #e5e5e5;
-  padding: 0 10px;
+  border-top: 1px solid #e5e5e5;
+  padding: 6px 20px 14px 20px;
 }
-
-.loading {
-  text-align: center;
-  padding-top: 300px;
+.replyUp {
+  margin-bottom: 22px;
 }
-
 .replyUp a:nth-of-type(2) {
-  margin-left: 0px;
+  margin-left: 6px;
+  margin-right: 8px;
   display: inline-block;
 }
 
 .topic_header {
   padding: 10px;
 }
+.upCount {
+  position: absolute;
+  left: 94%;
+  transform: translateY(100%);
+}
 
 .topic_title {
   font-size: 20px;
   font-weight: bold;
   padding-top: 8px;
+  padding-left: 6px;
 }
 
 .topic_header ul {
@@ -117,14 +121,20 @@ export default {
   display: inline-block;
   font-size: 12px;
   color: #838383;
+  margin: 6px 6px;
 }
 
 .topic_content {
-  border-top: 1px solid #e5e5e5;
-  padding: 0 10px;
+  padding: 10px 20px;
+  font-weight: 400;
+  line-height: 2;
 }
-
-.markdown-text img {
-  width: 92% !important;
+.replyContent {
+  line-height: 1.6;
+}
+</style>
+<style>
+.topic_content  img {
+  max-width: 100%;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="wrapper">
-    <img alt="CNODE社区" src="../assets/logo.svg" />
+    <img alt="CNODE社区" src="../assets/logo.svg" @click="home" />
     <ul>
-      <li><a href="#">首页</a></li>
-      <li><a href="#">新手入门</a></li>
-      <li><a href="#">API</a></li>
-      <li><a href="#">关于</a></li>
-      <li><a href="#">注册</a></li>
-      <li><a href="#">登录</a></li>
+      <li>
+        <a href="#">首页</a>
+      </li>
+      <li>
+        <a href="#">新手入门</a>
+      </li>
+      <li>
+        <a href="#">API</a>
+      </li>
+      <li>
+        <a href="#">关于</a>
+      </li>
+      <li>
+        <a href="#">注册</a>
+      </li>
+      <li>
+        <a href="#">登录</a>
+      </li>
     </ul>
   </div>
 </template>
@@ -16,34 +28,42 @@
 <script>
 export default {
   name: "Header",
+  methods: {
+    home() {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
 
 <style scoped>
-img{
+img {
   padding: 0 20px 4px;
   max-width: 200px;
 }
-.wrapper{
-  box-shadow: 0 1px 3px rgba(26,26,26,.1);
-  background-color:#24292E;
+img:hover {
+  cursor: pointer;
+}
+.wrapper {
+  box-shadow: 0 1px 3px rgba(26, 26, 26, 0.1);
+  background-color: #24292e;
   display: flex;
   height: 60px;
   justify-content: space-between;
   margin-bottom: 20px;
 }
-ul{
+ul {
   margin-right: 32px;
 }
-li{
+li {
   display: inline-block;
   padding: 24px 10px 0;
   font-size: 14px;
 }
-a{
+li > a {
   text-decoration: none;
-  color:#FFFFFF;
+  color: #ffffff;
   font-weight: 600;
 }
 </style>
