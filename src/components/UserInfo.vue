27c40d6dc7a0863d100832419cf9928e9e25919c
Flash <188813950@qq.com>
2019-11-30 23:39:20 +0800

    完成用户详情页

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -2,12 +2,14 @@
   <div class="UserInfo" v-if="user">
     <section class="mainInfo">
       <img :src="user.avatar_url" alt="头像" />
-      <span class="id">{{user.loginname}}</span>
-      <div class="score">积分：{{user.score}}</div>
-      <div class="signTime">注册时间：{{signTime}}</div>
+      <div>
+        <div class="id">{{user.loginname}}</div>
+        <div class="score">积分：{{user.score}}</div>
+        <div class="signTime">注册时间：{{signTime}}</div>
+      </div>
     </section>
     <section class="replies">
-      <span>最近参与的主题</span>
+      <div>最近参与的主题</div>
       <router-link
         :to="{name:'article',params:{id:item.id}}"
         v-for="item in user.recent_replies"
@@ -15,7 +17,7 @@
       >{{item.title}}</router-link>
     </section>
     <section class="topics">
-      <span>最近创建的主题</span>
+      <div>最近创建的主题</div>
       <router-link
         :to="{name:'article',params:{id:item.id}}"
         v-for="item in user.recent_topics"
@@ -46,17 +48,70 @@ export default {
 </script>
 
 <style scoped>
+.UserInfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.mainInfo {
+  font-weight: 450;
+  width: 855px;
+  margin: 14px 0;
+  background-color: #fff;
+  border: 1px solid #d1d5da;
+  border-radius: 10px;
+  display: flex;
+}
+.mainInfo > div > div {
+  margin: 16px 22px;
+}
 .replies {
+  margin: 14px 0;
+  padding: 0 0 0 8px;
+  width: 855px;
+  background-color: #fff;
+  border: 1px solid #d1d5da;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  min-height: 216px;
+}
+.replies > a {
+  margin: 8px;
+}
+.replies > :nth-last-child(1) {
+  margin: 8px 8px 16px;
+}
+.replies > div {
+  margin: 16px 8px 8px;
+  font-weight: 450;
+}
+.topics {
+  min-height: 216px;
+  font-size: 14px;
+  margin: 14px 0;
+  padding: 0 0 0 8px;
+  width: 855px;
+  background-color: #fff;
+  border: 1px solid #d1d5da;
+  border-radius: 10px;
   display: flex;
   flex-direction: column;
 }
-.topics {
-  display: flex;
-  flex-direction: column;
+.topics > a {
+  margin: 8px;
+}
+.topics > :nth-last-child(1) {
+  margin: 8px 8px 16px;
+}
+.topics > div {
+  margin: 16px 8px 8px;
+  font-weight: 450;
 }
 img {
   border-radius: 10px;
-  width: 100px;
+  width: 120px;
 }
 a {
   align-self: flex-start;
