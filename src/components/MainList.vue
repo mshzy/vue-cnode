be397d106b509e65df5e692b1381fe86f806ca9a
Flash <188813950@qq.com>
2019-12-02 03:19:24 +0800

    平滑滚动

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -82,7 +82,10 @@ export default {
         }
       }
       this.items = res.data.data
-      window.scrollTo(0, 0)
+      window.scrollTo({
+        top: 0,
+        behavior:'smooth'
+      })
     }
   },
 }
